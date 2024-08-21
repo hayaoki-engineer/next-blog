a947b3ea16a1c7fb18612b2a0e2725051652135f
Taisei <taisei.960420@gmail.com>
2024-08-22 06:36:29 +0900

    sectionの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
 import Image from 'next/image'
+import ArticleList from './components/ArticleList'
 
 export default function Home() {
   return (
-    <div>記事投稿</div>
-  )
+    <div className="md:flex">
+      <section className="w-full md:w-2/3 flex flex-col items-center px-3">
+        <ArticleList />
+      </section>
+
+      <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6">
+        <div className="bg-white shadow-md rounded p-4 mb-6 mt-4">
+          <h3 className="font-bold text-gray-900 mb-2">About Me</h3>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique.
+          </p>
+        </div>
+        <div className="bg-white shadow-md rounded p-4 mb-6 mt-4 w-full">
+          <h3 className="font-bold text-gray-900 mb-2">Category</h3>
+          <ul className="text-gray-600 mt-2">
+            <li>
+              <a href="#">Technology</a>
+            </li>
+            <li>
+              <a href="#">Automotive</a>
+            </li>
+            <li>
+              <a href="#">Finance</a>
+            </li>
+            <li>
+              <a href="#">Sports</a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+    </div>
+  );
 }
