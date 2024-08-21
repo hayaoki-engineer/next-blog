e27bf90a1c05d6d915ef629cac18ac5155113cb8
Taisei <taisei.960420@gmail.com>
2024-08-22 06:28:36 +0900

    ブログ共通フッターの作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-2 px-4 border-t'>
+      <small>@2023 shincode</small>
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './Header'
+import Footer from './Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className="container mx-auto bg-slate-700 text-slate-50">
         <Header />
         {children}
-        <footer>Footer</footer>
+        <Footer />
       </body>
     </html>
   )
