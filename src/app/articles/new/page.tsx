ec23d8c1acd16ca411f0044c5d5d38fbdf570587
Taisei <taisei.960420@gmail.com>
2024-08-27 05:44:01 +0900

    投稿用のローディングを実装

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+"use client"
+
+import { createArticle } from '@/blogAPI';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
 
 const CreateBlogPage = () => {
+  const router = useRouter();
+  const [id, setId] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    await createArticle(id, title, content);
+    setLoading(false);
+    router.push("/");
+    router.refresh();
+  }
+
   return (
-    <div className='min-h-screen py-8 px-4 md:px-12'>
-      <h2 className='text-2xl font-bold mb-4'>ブログ新規作成</h2>
-      <form className="bg-slate-200 p-6 rounded shadow-lg">
+    <div className="min-h-screen py-8 px-4 md:px-12">
+      <h2 className="text-2xl font-bold mb-4">ブログ新規作成</h2>
+      <form
+        className="bg-slate-200 p-6 rounded shadow-lg"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4">
           <label className="text-gray-700 text-sm font-bold mb-2">URL</label>
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setId(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -19,19 +42,27 @@ const CreateBlogPage = () => {
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="mb-4">
-          <label className="text-gray-700 text-sm font-bold mb-2">
-            本文
-          </label>
-          <textarea className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+          <label className="text-gray-700 text-sm font-bold mb-2">本文</label>
+          <textarea
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setContent(e.target.value)}
           />
         </div>
         <button
-          type='submit'
-          className='py-2 px-4 border rounded-md bg-orange-300'
-        >投稿</button>
+          type="submit"
+          className={`py-2 px-4 border rounded-md ${
+            loading
+              ? "bg-orange-300 cursor-not-allowed"
+              : "bg-orange-400 hover:bg-orange-500"
+          } text-white font-semibold focus:outline-none`}
+          disabled={loading}
+        >
+          投稿
+        </button>
       </form>
     </div>
   );
